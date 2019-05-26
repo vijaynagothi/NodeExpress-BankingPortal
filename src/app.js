686365d6e8437297aa8f434c0288df96692df548
Vijay Nagothi <vijay22448@gmail.com>
2019-05-26 12:57:18 +0530

    Completed module 2

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 const path = require('path');
 
 const port = process.env.PORT || '3000';
-
 const app = express();
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+const users = JSON.parse(userData);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,7 +19,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.render('index', { 'title': 'Index' });
+    res.render('index', { 'title': 'Account Summary', accounts });
+})
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+})
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+})
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
+})
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
 })
 
 
